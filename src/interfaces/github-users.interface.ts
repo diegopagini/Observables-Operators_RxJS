a4e98a875a1813a6c559651b8368e132a5525afb
Diego Pagini <diegopaginidev@gmail.com>
2021-01-28 07:36:56 -0300

    operadores de transformacion

---
+++ src/interfaces/github-users.interface.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+import { GithubUser } from './github-user.interface';
+
+export interface GithubUsersResp {
+	total_count: number;
+	incomplete_results: boolean;
+	items: GithubUser[];
+}
